efb3faf82407248bced52dbf4760ddd5d50120ff
claraAzul <clara.villalba67@gmail.com>
2026-04-29 11:04:35 +0200

    Recipes Mock

---
+++ src/mocks/RecipesMock.js
@@ -0,0 +1,38 @@
+export const recipesMock = [
+  {
+    idMeal: "1",
+    strMeal: "Chicken Curry",
+    strMealThumb: "https://www.themealdb.com/images/media/meals/1520084413.jpg",
+    ingredients: ["Chicken", "Curry powder", "Rice"]
+  },
+  {
+    idMeal: "2",
+    strMeal: "Spaghetti Bolognese",
+    strMealThumb: "https://www.themealdb.com/images/media/meals/sutysw1468247559.jpg",
+    ingredients: ["Pasta", "Minced meat", "Tomato sauce"]
+  },
+  {
+    idMeal: "3",
+    strMeal: "Grilled Salmon",
+    strMealThumb: "https://www.themealdb.com/images/media/meals/1548772327.jpg",
+    ingredients: ["Salmon", "Lemon", "Olive oil"]
+  },
+  {
+    idMeal: "4",
+    strMeal: "Beef Tacos",
+    strMealThumb: "https://www.themealdb.com/images/media/meals/1550441275.jpg",
+    ingredients: ["Beef", "Tortillas", "Cheese"]
+  },
+  {
+    idMeal: "5",
+    strMeal: "Vegetable Stir Fry",
+    strMealThumb: "https://www.themealdb.com/images/media/meals/wvqpwt1468339226.jpg",
+    ingredients: ["Broccoli", "Carrots", "Soy sauce"]
+  },
+  {
+    idMeal: "6",
+    strMeal: "Pancakes",
+    strMealThumb: "https://www.themealdb.com/images/media/meals/rwuyqx1511383174.jpg",
+    ingredients: ["Flour", "Milk", "Eggs"]
+  }
+];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,25 @@
+import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { setRecipes } from "../core/Actions/RecipeActions";
 import { searchRecipes } from "../api/RecipesApi";
+
 import Header from "../components/Header";
 import RecipeList from "../components/RecipeList";
 import Navbar from "../components/NavBar";
+
+import { recipesMock } from "../mocks/RecipesMock";
 
 import "../styles/HomeLayout.css";
 
 function Home() {
   const dispatch = useDispatch();
 
+
+  useEffect(() => {
+    dispatch(setRecipes(recipesMock));
+  }, [dispatch]);
+
+  
   const handleSearch = async (ingredient) => {
     const results = await searchRecipes(ingredient);
     dispatch(setRecipes(results));
@@ -17,17 +27,12 @@ function Home() {
 
   return (
     <div className="home-container">
-
       <div className="content">
-
         <Header onSearch={handleSearch} />
-
         <RecipeList />
-
       </div>
 
       <Navbar />
-
     </div>
   );
 }
